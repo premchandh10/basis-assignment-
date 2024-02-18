ea451d937967df1213be63a52a8b902e53ad3dd4
premchandh10 <160237789+premchandh10@users.noreply.github.com>
2024-02-18 22:58:25 +0530

    saved assignment files are added

---
+++ Basics.cpp
@@ -0,0 +1,84 @@
+﻿                                                            Assignments
+
+
+                                                          C++ Assignments 1
+ 
+
+Q1. How can you output "Physics" and "Wallah" in two different lines in C++? 
+
+(A)
+
+  # include <iostream>
+using namespace std ;
+        int main(){
+                  cout<<"Physics"<<endl;
+                        cout<<"Wallah"<<endl;
+                                          Return 0;
+        }
+
+
+Q2. Write a program to print 10 using 2 positive numbers less than 6 in C++ as output. 
+
+(A) 
+
+# include <iostream>
+  using namespace std;
+  int main (){
+             int a = 5;
+                int b = 5;
+                     int sum = 5+5;
+                             cout << sum ;
+                                        return 0;
+  }   
+                                                                                                                                                                                                       int main(){                                                                                                        int a = 5;                                                                                                        int b = 5;                                                                                                        int sum=5+5;                                                                                                cout <<sum;                                                                                                        return 0;                                                        
+}                 
+Q3. How much space does the following data types take? 
+ int  - 4
+ bool - 1
+ float - 4
+
+
+Q4. What is the output of this program? 
+int main() { 
+int a = 4; 
+int b = 5; 
+a++, b--; 
+cout << ++a << " " << b--; 
+} 
+
+
+(A) 6 4
+
+
+Q5. Write a program to find the circumference of a circle with radius 10 in C++.   
+
+(A)
+
+# include<iostream>
+      
+using namespace std;
+
+    int main(){
+          int radius = 10;
+                  float pi = 3.14;
+                        float circumference = 2*pi*radius;    
+                                 cout<<circumference;
+                                 
+                          return 0;
+}                                                                                                                                                             (A)                                                                                                                        #include<iostream>                                                                                                using namespace std;                                                                                                int main(){                                                                                                        int radius = 10;                                                                                                        float pi = 3.14                                                                                                        float circumference = 2*pi*radius                                                                                cout<<circumference;                                                                                                return 0;                                                                                                        }                                                                                                                         
+
+
+Q6. How many of these can be a variable name ? 
+ 01Pwskills  (A) no
+ FLOAT  (A) yes
+ int  (A) yes
+ FLOAT  (A) no
+ You will succeed  (A) no
+
+(A). 2 
+
+
+
+
+Note:- Please try to invest time doing the assignments which are necessary to build a strongfoundation. Do not  directly Copy Paste using Google or ChatGPT. Please use your brain. 
+Java + DSA C++ & DSA
